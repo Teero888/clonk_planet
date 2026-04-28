04bf10329bf28aaefa90d4a3d097d8897224bfd6
Teero888 <teero484@gmail.com>
2026-04-28 11:32:13 +0200

    move external libs to external

--- scripts/fix_style.py
+++ scripts/fix_style.py
@@ -7,7 +7,7 @@ import os
 def format_files():
     files_to_format = []
     extensions = ["**/*.cpp", "**/*.c", "**/*.h", "**/*.glsl"]
-    excluded_dirs = ["libs", "build"]
+    excluded_dirs = ["standard/inc/external","standard/inc/miniaudio.h", "build", "original_source"]
 
     for ext in extensions:
         files_to_format.extend(glob.glob(ext, recursive=True))
@@ -17,7 +17,7 @@ def format_files():
         if any(excluded_dir in path_parts for excluded_dir in excluded_dirs):
             continue
         print(f"Formatting {file}...")
-        subprocess.run(["clang-format", "-i", file])
+        subprocess.run(["clang-format", "--style=LLVM", "-i", file])
 
 if __name__ == "__main__":
     format_files()
