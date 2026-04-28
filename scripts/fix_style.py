7d68aa3268d412ef600814ed126020eb77ab568d
Teero888 <teero484@gmail.com>
2026-04-28 11:38:52 +0200

    update fix style

--- scripts/fix_style.py
+++ scripts/fix_style.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
 import subprocess
-import glob
-import os
+from pathlib import Path
 
 def format_files():
-    files_to_format = []
-    extensions = ["**/*.cpp", "**/*.c", "**/*.h", "**/*.glsl"]
-    excluded_dirs = ["standard/inc/external","standard/inc/miniaudio.h", "build", "original_source"]
+    extensions = ["*.cpp", "*.c", "*.h", "*.glsl"]
+    excluded_dirs = [
+        Path("standard/inc/external"),
+        Path("standard/zlib"),
+        Path("standard/unibase"),
+        Path("build"),
+        Path("original_source")
+    ]
 
     for ext in extensions:
-        files_to_format.extend(glob.glob(ext, recursive=True))
+        for file_path in Path(".").rglob(ext):
+            if any(excl in file_path.parents for excl in excluded_dirs):
+                continue
 
-    for file in files_to_format:
-        path_parts = os.path.normpath(file).split(os.sep)
-        if any(excluded_dir in path_parts for excluded_dir in excluded_dirs):
-            continue
-        print(f"Formatting {file}...")
-        subprocess.run(["clang-format", "--style=LLVM", "-i", file])
+            print(f"Formatting {file_path}...")
+            subprocess.run(["clang-format", "--style=LLVM", "-i", str(file_path)])
 
 if __name__ == "__main__":
     format_files()
